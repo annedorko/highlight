117993a88da516012eeb5b7b8309178991c88227
Anne Dorko <anne@dorko.tv>
2021-02-10 00:03:47 +0100

    Added Tailwind, fixed reloading issue

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Portfolio Generator
 
-An experiment building a lightweight static site generator with python to build a beautiful resume and portfolio site targeted for different ideal job titles. 
+An experiment building a lightweight static site generator with python to build a beautiful resume and portfolio site targeted for different ideal job titles.
+
+## Usage
+
+`python site.py`
--- site.py
+++ site.py
@@ -1,7 +1,8 @@
 # Tutorial
 # https://blog.naveeraashraf.com/posts/make-static-site-generator-with-python-2/
+import os
 import markdown
-from jinja2 import Environment, PackageLoader
+from jinja2 import Environment, FileSystemLoader
 
 # Convert contents to Markdown
 with open('content/page.md', 'r') as file:
@@ -9,7 +10,7 @@ with open('content/page.md', 'r') as file:
     md = markdown.Markdown(extensions=['meta'])
     html = md.convert(file.read())
 
-    env = Environment(loader=PackageLoader('main', 'templates'))
+    env = Environment(loader=FileSystemLoader('templates'))
     template = env.get_template('wrapper.html')
 
     data = {
@@ -17,7 +18,10 @@ with open('content/page.md', 'r') as file:
         'title': md.Meta.get('title')[0],
         'date': md.Meta.get('date')[0]
     }
-
     page = template.render(post=data)
     with open('site/index.html', 'w') as file:
         file.write(page)
+
+# Render CSS
+os.system(
+    'npx tailwindcss-cli@latest build ./assets/css/styles.css -o site/css/tailwind.css')
