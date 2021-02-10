234b0faddb516bd1217f67e0dde78e40b823c1a8
Anne Dorko <anne@dorko.tv>
2021-02-10 23:35:44 +0100

    Page generation fixed #2, flexible templates

--- app/write.py
+++ app/write.py
@@ -1,3 +1,4 @@
+import os
 import markdown
 from jinja2 import Environment, FileSystemLoader
 from watchdog.observers import Observer
@@ -15,19 +16,47 @@ class RegenerateSite(LoggingEventHandler):
 
 
 def generate_site(site):
-    # Convert contents to Markdown
-    with open('pages/page.md', 'r') as file:
-        # Meta returns as a Dictionary with lists
-        md = markdown.Markdown(extensions=['meta'])
-        html = md.convert(file.read())
+    env = Environment(loader=FileSystemLoader('templates'))
+    PAGES = {}
+    for page in os.listdir('pages'):
+        file_path = os.path.join('pages', page)
 
-        env = Environment(loader=FileSystemLoader('templates'))
-        template = env.get_template('wrapper.html')
+        with open(file_path, 'r') as file:
+            md = markdown.Markdown(extensions=['meta'])
+            # Process HTML
+            html = md.convert(file.read())
+            # Process metadata, return each as string
+            meta = {}
+            for m in md.Meta:
+                meta[m] = md.Meta[m][0]
+            # Clean filename
+            filename_ext = os.path.basename(file.name)
+            filename = os.path.splitext(filename_ext)[0]
+            # Add to overall page collection
+            PAGES[page] = {
+                'filename': filename,
+                'content': html,
+                'meta': meta,
+            }
 
-        data = {
-            'content': html,
-            'title': md.Meta.get('title')[0]
-        }
-        page = template.render(post=data, person=site['person'])
-        with open('site/index.html', 'w') as file:
+    for p in PAGES:
+        slug = PAGES[p].get('filename')
+        content = PAGES[p].get('content')
+        meta = PAGES[p].get('meta')
+        # Set template
+        set_template = 'page'
+        if 'template' in meta:
+            set_template = meta.get('template')
+        template = env.get_template(set_template + '.html')
+        # Write page to template
+        page = template.render(
+            page=content,
+            meta=meta,
+            person=site['person']
+        )
+        # Set slug
+        if 'slug' in meta:
+            slug = meta.slug
+        # Save page
+        with open('site/' + slug + '.html', 'w') as file:
             file.write(page)
