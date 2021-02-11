b0e2227e9a5e28b387ae85569aa56884638ea64d
Anne Dorko <anne@dorko.tv>
2021-02-11 15:24:12 +0100

    Prepared for print - Cleaned up sidebar styles - Reordered education and skills - Cleaned personal data to fit one page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'print': {'raw': 'print'},
+      }
+    },
   },
   variants: {
     extend: {},
