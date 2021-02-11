ba4478f7b445340a45a1577ddf30e44b2aceddd0
Anne Dorko <anne@dorko.tv>
2021-02-11 23:52:55 +0100

    Adding print styles to production

--- tailwind.prod.config.js
+++ tailwind.prod.config.js
@@ -7,7 +7,11 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'print': {'raw': 'print'},
+      }
+    },
   },
   variants: {
     extend: {},
