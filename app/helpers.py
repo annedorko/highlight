a225ac660c6ad8de2c1945aed0c5ce8c64819191
Anne Dorko <anne@dorko.tv>
2021-02-11 22:28:38 +0100

    Page design and full resume generation intact closes #5

--- app/helpers.py
+++ app/helpers.py
@@ -5,6 +5,7 @@ import markdown
 import yaml
 import datetime
 import math
+from operator import itemgetter
 
 
 def date_diff(older, newer):
@@ -16,7 +17,10 @@ def date_diff(older, newer):
     0 days 4 hours 3 minutes returns '4 hours', etc...
     """
 
-    timeDiff = newer - older
+    # Min-max month estimates
+    # Oldest month starts on the 1st
+    # Newest month starts on the 28th
+    timeDiff = newer.replace(day=28) - older.replace(day=1)
     days = timeDiff.days
     hours = timeDiff.seconds / 3600
     minutes = timeDiff.seconds % 3600 / 60
@@ -81,72 +85,101 @@ def date_diff(older, newer):
 
 
 def load_history(role):
-    HISTORY = {}
+    PAST = {}
+    CURRENT = {}
+    # Set file names
+    LIST = {}
     for page in os.listdir('resume/history'):
-        file_path = os.path.join('resume/history', page)
-        with open(file_path, 'r') as stream:
+        LIST[page] = os.path.join('resume/history', page)
+    # Read YAML from each file
+    for item in LIST:
+        # Load experience
+        experience = False
+        with open(LIST[item], 'r') as stream:
             try:
-                exp = yaml.safe_load(stream)
-                includes = exp.get('include')[0]
-                look_for = ['All', role]
-                # Turn list into dictionary
-                if any(search in includes for search in look_for):
-                    # Process YAML data to pass onto page
-                    # titles (default, Role)
-                    # company
-                    # location
-                    # url
-                    # type
-                    # dates (YYYY-MM): start, end
-                    # skills: base, default, Role
-                    # descriptions: default, Role
-                    title = exp.get('titles')[role] if role in exp.get(
-                        'titles') else exp.get('titles')['default']
-                    base_skills = exp.get('skills')['base']
-                    special_skills = exp.get('skills')[role] if role in exp.get(
-                        'skills') else exp.get('skills')['default']
-                    skills = base_skills + special_skills
-                    description = exp.get('descriptions')[role] if role in exp.get(
-                        'descriptions') else exp.get('descriptions')['default']
-                    # TODO: start dates at first of month, end dates at last day of month
-                    start = ''
-                    end = 'Present'
-                    if 'dates' in exp:
-                        if 'start' in exp.get('dates'):
-                            start_dt = exp.get('dates')['start']
-                            start_dt_obj = datetime.datetime.strptime(
-                                start_dt, '%Y-%m')
-                            start_formatted = datetime.datetime.strftime(
-                                start_dt_obj, '%b %Y')
-                            start = start_formatted
-                        if 'end' in exp.get('dates') and not exp.get('dates')['end'] == 'Present':
-                            end_dt = exp.get('dates')['end']
-                            end_dt_obj = datetime.datetime.strptime(
-                                end_dt, '%Y-%m')
-                            end_formatted = datetime.datetime.strftime(
-                                end_dt_obj, '%b %Y')
-                            end = end_formatted
-                            length = date_diff(start_dt_obj, end_dt_obj)
-                        else:
-                            length = date_diff(start_dt_obj, datetime.now())
-
-                    HISTORY[page] = {
-                        'role': title,
-                        'company': exp.get('company') if 'company' in exp else '',
-                        'location': exp.get('location') if 'location' in exp else '',
-                        'url': exp.get('url') if 'url' in exp else '',
-                        'type': exp.get('type') if 'type' in exp else '',
-                        'skills': skills,
-                        'description': description,
-                        'start': start,
-                        'end': end,
-                        'length': length
-                    }
-                    # End if any
+                experience = yaml.safe_load(stream)
             except yaml.YAMLError as exc:
-                return
-    # TODO sort relavent history by date
-    return HISTORY
+                print(exc)
+        # Process experience
+        include = False
+        if not experience is False:
+            includes = experience.get('include')[0]
+            look_for = ['All', role]
+            if any(search in includes for search in look_for):
+                include = True
+        # If including, carry on
+        if include is True:
+            # Process YAML data to pass onto page
+            # titles (default, Role)
+            # company
+            # location
+            # url
+            # type
+            # dates (YYYY-MM): start, end
+            # skills: base, default, Role
+            # descriptions: default, Role
+            title = experience.get('titles')[role] if role in experience.get(
+                'titles') else experience.get('titles')['default']
+            base_skills = experience.get('skills')['base']
+            special_skills = experience.get('skills')[role] if role in experience.get(
+                'skills') else experience.get('skills')['default']
+            skills = base_skills + special_skills
+            description = experience.get('descriptions')[role] if role in experience.get(
+                'descriptions') else experience.get('descriptions')['default']
+            start = ''
+            end = 'Present'
+            sort = datetime.datetime.now()
+            if 'dates' in experience:
+                if 'start' in experience.get('dates'):
+                    start_dt = experience.get('dates')['start']
+                    start_dt_obj = datetime.datetime.strptime(
+                        start_dt, '%Y-%m')
+                    start_formatted = datetime.datetime.strftime(
+                        start_dt_obj, '%b %Y')
+                    start = start_formatted
+                if 'end' in experience.get('dates') and not experience.get('dates')['end'] == 'Present':
+                    end_dt = experience.get('dates')['end']
+                    end_dt_obj = datetime.datetime.strptime(
+                        end_dt, '%Y-%m')
+                    end_formatted = datetime.datetime.strftime(
+                        end_dt_obj, '%b %Y')
+                    end = end_formatted
+                    sort = start_dt_obj
+                    length = date_diff(start_dt_obj, end_dt_obj)
+                else:
+                    sort = start_dt_obj + datetime.timedelta(days=2)
+                    length = date_diff(
+                        start_dt_obj, datetime.datetime.now())
+
+            bundle = {
+                'role': title,
+                'company': experience.get('company') if 'company' in experience else '',
+                'location': experience.get('location') if 'location' in experience else '',
+                'url': experience.get('url') if 'url' in experience else '',
+                'type': experience.get('type') if 'type' in experience else '',
+                'skills': skills,
+                'description': description,
+                'sort': sort,
+                'start': start,
+                'end': end,
+                'length': length
+            }
+            if bundle['end'] == 'Present':
+                CURRENT[item] = bundle
+            else:
+                PAST[item] = bundle
+                # End inclusion if statement
+                # TODO sort relavent history by date
+    WORK = {}
+    sort_past = sorted(
+        PAST.items(), key=lambda x: x[1]['sort'], reverse=True)
+    sort_current = sorted(
+        CURRENT.items(), key=lambda x: x[1]['sort'])
+    for i, h in sort_current:
+        WORK[i] = h
+    for i, h in sort_past:
+        WORK[i] = h
+    return WORK
 
 
 def load_pages():
